dcf5d5745e47743236e78f8e2011be8b5f1d11ac
pirabaud <rabaudp@gmail.com>
2025-03-17 12:04:53 +0100

    script

--- p1/scriptS.sh
+++ p1/scriptS.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
-sudo apt-get install curl
+sudo sed -i -e 's/us.archive.ubuntu.com/archive.ubuntu.com/g' /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install -y curl
 curl -sfL https://get.k3s.io/ | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server" sh -s - --flannel-iface=eth1
-
+ExecStart=/usr/local/bin/k3s server --kubelet-arg="cni-iface=eth1"
--- p1/scriptSW.sh
+++ p1/scriptSW.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-sudo apt-get install curl
+sudo sed -i -e 's/us.archive.ubuntu.com/archive.ubuntu.com/g' /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install -y curl
 curl -sfL https://get.k3s.io/ | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="agent" sh -s - --flannel-iface=eth1
+ExecStart=/usr/local/bin/k3s agent --kubelet-arg="cni-iface=eth1"
