7d811644900fb299608a235fb50ffcf63f46f3d3
painpats <cbourrewe@gmail.com>
2025-03-17 16:59:49 +0100

    scripts that don't work but we are close to a good result !

--- p1/scriptS.sh
+++ p1/scriptS.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install curl
+sudo sed -i -e 's/us.archive.ubuntu.com/archive.ubuntu.com/g' /etc/apt/sources.list
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install -y curl
 
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --flannel-iface eth1" sh -
--- p1/scriptSW.sh
+++ p1/scriptSW.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-IP_S= 192.168.56.110
+IP_S="192.168.56.110"
 K3S_TOKEN=$(cat /var/lib/rancher/k3s/server/node-token)
 
-sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install curl
+sudo sed -i -e 's/us.archive.ubuntu.com/archive.ubuntu.com/g' /etc/apt/sources.list
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install -y curl
 
 curl -sfL https://get.k3s.io | K3S_URL=https://$IP_S:6443 K3S_TOKEN=$K3S_TOKEN sh -
 
