7a2e141816a461a1836724439e5df44f841121f6
painpats <cbourrewe@gmail.com>
2025-03-17 13:33:01 +0100

    testing scripts

--- p1/scriptS.sh
+++ p1/scriptS.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+
 sudo sed -i -e 's/us.archive.ubuntu.com/archive.ubuntu.com/g' /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get install -y curl
-curl -sfL https://get.k3s.io/ | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server" sh -s - --flannel-iface=eth1
-ExecStart=/usr/local/bin/k3s server --kubelet-arg="cni-iface=eth1"
+
+IP_ADDRESS="192.168.56.110"
+NETMASK="255.255.255.0"
+
+sudo ip addr add $IP_ADDRESS/$NETMASK dev eth1 || true
+sudo ip link set eth1 up
+
+sudo swapoff -a
+sudo sed -i '/ swap / s/^/#/' /etc/fstab
+
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --bind-address=$IP_ADDRESS --node-ip=$IP_ADDRESS" sh -
+
--- p1/scriptSW.sh
+++ p1/scriptSW.sh
@@ -3,5 +3,24 @@
 sudo sed -i -e 's/us.archive.ubuntu.com/archive.ubuntu.com/g' /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get install -y curl
-curl -sfL https://get.k3s.io/ | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="agent" sh -s - --flannel-iface=eth1
-ExecStart=/usr/local/bin/k3s agent --kubelet-arg="cni-iface=eth1"
+
+AGENT_IP="192.168.56.111"
+NETMASK="255.255.255.0"
+
+sudo ip addr add $AGENT_IP/$NETMASK dev eth1 || true
+sudo ip link set eth1 up
+
+sudo swapoff -a
+sudo sed -i '/ swap / s/^/#/' /etc/fstab
+
+SERVER_IP="192.168.56.110"
+TOKEN_FILE="/vagrant/node-token"
+
+echo "Attente du fichier token..."
+while [ ! -f "$TOKEN_FILE" ]; do
+  sleep 2
+done
+
+K3S_TOKEN=$(cat "$TOKEN_FILE")
+
+curl -sfL https://get.k3s.io | K3S_URL="https://$SERVER_IP:6443" K3S_TOKEN="$K3S_TOKEN" sh -
