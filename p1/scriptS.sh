812f63976f9b1b15166f668f7736bdc1b90b302a
painpats <cbourrewe@gmail.com>
2025-03-18 11:50:54 +0100

    near near near

--- p1/scriptS.sh
+++ p1/scriptS.sh
@@ -4,4 +4,5 @@ sudo sed -i -e 's/us.archive.ubuntu.com/archive.ubuntu.com/g' /etc/apt/sources.l
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install -y curl
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --flannel-iface eth1" sh -
+IP_S="192.168.56.110"
+curl -sfL https://get.k3s.io/ | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server --node-ip $IP_S --flannel-iface eth1" sh - 
