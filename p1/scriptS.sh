ac5eb749ba16373e8887dfcc7ab74ee9a5924346
painpats <cbourrewe@gmail.com>
2025-03-19 11:41:09 +0100

    scripts: functionnal, with 2 token method

--- p1/scriptS.sh
+++ p1/scriptS.sh
@@ -4,7 +4,7 @@ sudo sed -i -e 's/us.archive.ubuntu.com/archive.ubuntu.com/g' /etc/apt/sources.l
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install -y curl
 
-curl -sfL https://get.k3s.io/ | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server --flannel-iface eth1" K3S_TOKEN=mytoken sh - 
-# cd /vagrant
-# touch token
-# sudo cp /var/lib/rancher/k3s/server/node-token token
+curl -sfL https://get.k3s.io/ | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server --flannel-iface eth1" sh - 
+sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/token
+
+# curl -sfL https://get.k3s.io/ | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server --flannel-iface eth1" K3S_TOKEN=mytoken sh -
--- p1/scriptSW.sh
+++ p1/scriptSW.sh
@@ -4,5 +4,7 @@ sudo sed -i -e 's/us.archive.ubuntu.com/archive.ubuntu.com/g' /etc/apt/sources.l
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install -y curl
 
-# IP_S="192.168.56.110"  K3S_TOKEN_FILE="/vagrant/token"
-curl -sfL https://get.k3s.io/ | INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 --flannel-iface eth1 --token mytoken" sh -
+IP_S="192.168.56.110"  
+export K3S_TOKEN_FILE="/vagrant/token"
+curl -sfL https://get.k3s.io/ | INSTALL_K3S_EXEC="agent --server https://$IP_S:6443 --flannel-iface eth1 --token $K3S_TOKEN_FILE" sh -
+# curl -sfL https://get.k3s.io/ | INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 --flannel-iface eth1 --token mytoken" sh -
